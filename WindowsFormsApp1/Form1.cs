64a1e715ab6aa74ac82e23c3df62bd9ee6637eb7
龚杰 <gongjie@qq.com>
2017-09-02 16:50:47 +0800

    1111

--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -16,17 +16,21 @@ namespace WindowsFormsApp1
 {
     public partial class Form1 : Form
     {
-     
+
+        const string HOSTADDRESS = "172.16.1.185";
+        const int HOSTPORT = 19850;
         public Form1()
         {
             InitializeComponent();
-            UdpClient udpClient = new System.Net.Sockets.UdpClient(19850);
-            UdpClient udpServer = new System.Net.Sockets.UdpClient(19851);
-            udpServer.BeginReceive(requestCallbacServer, udpServer);
+            UdpClient udpClient = new System.Net.Sockets.UdpClient(19851);
+
+            //UdpClient udpServer = new System.Net.Sockets.UdpClient(19851);
+            //udpClient.Connect(HOSTADDRESS, 19851);
+            // udpServer.BeginReceive(requestCallbacServer, udpServer);
             udpClient.BeginReceive(requestCallbacClient, udpClient);
             Send = new Action<byte[]>(o =>
             {
-                udpClient.SendAsync(o, o.Length, "localhost", 19851);
+                udpClient.SendAsync(o, o.Length, HOSTADDRESS, HOSTPORT);
             });
         }
         public Action<byte[]> Send { get; set; }
@@ -35,8 +39,8 @@ namespace WindowsFormsApp1
             UdpClient client = ar.AsyncState as UdpClient;
             if (ar.IsCompleted)
             {
-                System.Net.IPEndPoint ep=null;
-                var bytes= client.EndReceive(ar, ref ep);
+                System.Net.IPEndPoint ep = null;
+                var bytes = client.EndReceive(ar, ref ep);
                 client.SendAsync(bytes, bytes.Length, ep);
             }
             client.BeginReceive(requestCallbacServer, client);
@@ -48,7 +52,14 @@ namespace WindowsFormsApp1
             {
                 System.Net.IPEndPoint ep = null;
                 var bytes = client.EndReceive(ar, ref ep);
-                Decode(bytes);
+                try
+                {
+                    Decode(bytes);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
             }
             client.BeginReceive(requestCallbacClient, client);
         }
@@ -62,63 +73,38 @@ namespace WindowsFormsApp1
             recorder.WaveFormat = new WaveFormat(8000, 16, 1);
             recorder.BufferMilliseconds = 40;
             recorder.DataAvailable += WaveIn_DataAvailable;
-
-
-
-            // set up our signal chain
             bufferedWaveProvider = new BufferedWaveProvider(recorder.WaveFormat);
-
-            //bufferedWaveProvider.DiscardOnBufferOverflow = true;
-            // set up playback
             player = new WaveOut();
             player.Init(bufferedWaveProvider);
-
-            // begin playback & record
             player.Play();
             recorder.StartRecording();
         }
         SpeexEncoder encoder = new SpeexEncoder(BandMode.Wide);
         private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
         {
-            // convert to short
             short[] data = new short[e.BytesRecorded / 2];
             Buffer.BlockCopy(e.Buffer, 0, data, 0, e.BytesRecorded);
             var encodedData = new byte[e.BytesRecorded];
-            // note: the number of samples per frame must be a multiple of encoder.FrameSize
             var encodedBytes = encoder.Encode(data, 0, data.Length, encodedData, 0, encodedData.Length);
             if (encodedBytes != 0)
             {
-                // todo: do something with the encoded data
             }
-
             var sendData = new byte[encodedBytes];
             Buffer.BlockCopy(encodedData, 0, sendData, 0, encodedBytes);
-
-
-            // Decode(sendData);
             Send(sendData);
-
             Console.WriteLine($"{e.BytesRecorded} => {encodedBytes}  {DateTime.Now}");
-            //bufferedWaveProvider.AddSamples(e.Buffer, 0, e.BytesRecorded); 
         }
 
         SpeexDecoder decoder = new SpeexDecoder(BandMode.Wide);
-
-
         private void Decode(byte[] encodedData)
-        { 
+        {
             var data = new byte[640];
             Buffer.BlockCopy(encodedData, 0, data, 0, encodedData.Length);
-            short[] decodedFrame = new short[320]; // should be the same number of samples as on the capturing side
+            short[] decodedFrame = new short[320];
             decoder.Decode(encodedData, 0, encodedData.Length, decodedFrame, 0, false);
             var playData = new byte[640];
             Buffer.BlockCopy(decodedFrame, 0, playData, 0, 640);
             bufferedWaveProvider.AddSamples(playData, 0, playData.Length);
         }
-
     }
-
-
-
-
 }
